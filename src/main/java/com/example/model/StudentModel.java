27c4eadb447c79fd9873b35d8f56bb1bba054335
Ristya Nintyana <rstya.ristya@gmail.com>
2017-10-07 23:33:24 +0700

    tutor5

--- src/main/java/com/example/model/StudentModel.java
+++ src/main/java/com/example/model/StudentModel.java
@@ -1,5 +1,7 @@
 package com.example.model;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +14,6 @@ public class StudentModel
     private String npm;
     private String name;
     private double gpa;
+    private List<CourseModel> courses;
 
 }
